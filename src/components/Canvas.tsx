2087721f9222fe88946346966a2d21e471523f47
sophialittlejohn <littlejohn.sophia@gmail.com>
2020-04-19 01:00:20 +0200

    Convert Canvas to typescript

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,16 +1,22 @@
 import React, { useRef, useState, useEffect } from "react";
 import { drawCanvas } from "../utils/helpers";
-import { getTime } from "../utils/getTime";
+import { getTime, Time } from "../utils/getTime";
+import { useConfig } from "./ProductConfig";
 
-export const Canvas = (props) => {
-  const canvasRef = useRef(null);
-  const context = useRef({ current: null });
-  const [time, setTime] = useState(null);
+interface CanvasProps {}
+
+export const Canvas: React.FC<CanvasProps> = (props) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const context = useRef<CanvasRenderingContext2D | null>(null);
+
+  const [time, setTime] = useState<null | Time>(null);
+
+  const { zoom, colorChoice } = useConfig();
 
   useEffect(() => {
     setTime(getTime());
     window.setInterval(() => setTime(getTime()), 1000);
-    if (canvasRef && canvasRef.current) {
+    if (canvasRef && canvasRef.current && context && context) {
       const context2d = canvasRef.current.getContext("2d");
       context.current = context2d;
     }
@@ -20,9 +26,9 @@ export const Canvas = (props) => {
     if (canvasRef && canvasRef.current && time && context.current) {
       const { width, height } = canvasRef.current;
       context.current.clearRect(0, 0, width, height);
-      drawCanvas(context.current, props, time, width);
+      drawCanvas(context.current, { zoom, colors: colorChoice }, time, width);
     }
-  }, [time, props]);
+  }, [time, zoom, colorChoice]);
 
   return <canvas ref={canvasRef} width={250} height={250} />;
 };
